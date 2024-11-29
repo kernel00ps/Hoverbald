e2bb5c51dc58de5a67cb2b483f035d8d32622214
BYK27 <vuk.aleksijevic2001@gmail.com>
2024-11-30 00:48:43 +0100

    Pick up energy prototype

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -2,5 +2,6 @@ extends Node
 
 const CAMERA_MOTION_BASE_SPEED: float = 100
 const MAX_FUEL = 100
+const PICK_UP_ENERGY_VALUE: float = 10
 var camera_speed_modifier: float = 1
 var current_fuel: float = 100
