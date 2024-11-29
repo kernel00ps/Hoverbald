1d587c3e3fd404cd12f2215f4841d26072f1e7f4
BYK27 <vuk.aleksijevic2001@gmail.com>
2024-11-29 18:52:03 +0100

    Player fixed. Moving bg fixed

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,19 +1,25 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 80.0
 const JUMP_VELOCITY = -10.0
 
 func _ready() -> void:
 	velocity = Vector2.ZERO
-	position.x = -200
+	position.x -= 50
 
 func _physics_process(delta: float) -> void:
-	
-	#velocity.x = Globals.CAMERA_MOTION_BASE_SPEED * Globals.camera_speed_modifier * delta
+
 	velocity += get_gravity() * delta
-	print(position.x)
-	# Add the gravity.
+
 	if Input.is_action_pressed("UP"):
 		velocity.y += JUMP_VELOCITY
+	
+	var direction = Input.get_axis("LEFT", "RIGHT")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
 	move_and_slide()
 	
