6c3223295c7864bd7dba701a6a6834abccaae3c5
kernel00ps <davidanderson27@protonmail.com>
2024-11-29 23:16:56 +0100

    added fuel bar (again)

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -1,4 +1,6 @@
 extends Node
 
 const CAMERA_MOTION_BASE_SPEED: float = 100
-@export var camera_speed_modifier: float = 1
+const MAX_FUEL = 100
+var camera_speed_modifier: float = 1
+var current_fuel: float = 100
