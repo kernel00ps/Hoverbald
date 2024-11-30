fffa78538829fcd35b6f0b39724c2a53a11b0382
BYK27 <vuk.aleksijevic2001@gmail.com>
2024-11-30 19:34:50 +0100

    CRT shader

---
+++ scripts/crt.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+var viewport_size: Vector2
+var viewport_width: float
+var viewport_height: float
+
+func _ready() -> void:
+	viewport_size = get_viewport().get_visible_rect().size
+	viewport_width = viewport_size.x
+	viewport_height = viewport_size.y
+	
+func _process(delta: float) -> void:
+	pass
