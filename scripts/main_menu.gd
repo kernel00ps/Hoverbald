9ba5918137d944c3ae875e9b930524c38f5bce2d
BYK27 <vuk.aleksijevic2001@gmail.com>
2024-12-01 15:41:55 +0100

    Decreased particles

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _ready() -> void:
 	$ShonjaButton.modulate = Color(1, 1, 1, 0)
-
+	
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
@@ -22,7 +22,6 @@ func _on_credits_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
-
 
 func _on_shonja_button_pressed() -> void:
 	if Globals.is_sound_on:
