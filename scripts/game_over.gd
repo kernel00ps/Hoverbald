b3a5b7370d4d6063cf8b7584069d855bbd4e7b54
kernel00ps <davidanderson27@protonmail.com>
2024-12-01 17:25:37 +0100

    reset vars cleanup

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -7,21 +7,21 @@ func _ready() -> void:
 func _on_retry_pressed() -> void:
 	get_tree().paused = false
 	get_tree().reload_current_scene()
-	Globals.current_hp = Globals.MAX_HP
-	Globals.current_fuel = Globals.MAX_FUEL
-	Globals.CHEAT_INVINCIBLE = false
+	reset_vars()
 	
 func game_over() -> void:
 	Globals.camera_speed_modifier = 1
-	Globals.CHEAT_INVINCIBLE = false
-	Globals.current_hp = Globals.MAX_HP
+	reset_vars()
 	get_tree().paused = true
 	get_parent().play_gameover()
 	self.show()
 
 func _on_main_menu_pressed() -> void:
 	get_tree().paused = false
+	reset_vars()
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	
+func reset_vars() -> void:
 	Globals.current_hp = Globals.MAX_HP
 	Globals.current_fuel = Globals.MAX_FUEL
 	Globals.CHEAT_INVINCIBLE = false
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
